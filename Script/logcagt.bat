c5752e263a03818fbc563468cb0dd3944d42f07d
backryun <sungbin0718@naver.com>
2019-06-22 00:56:37 +0900

    1차 정리

--- Script/logcagt.bat
+++ Script/logcagt.bat
@@ -10,21 +10,28 @@ echo 2.특정 패키지 로그 추출
 echo.
 echo 3.선택한 레밸 로그 추출
 echo.
-echo 4.
+echo 4.로그추출+파일저장
 echo.
+echo 5.나가기
 set main=
 set /p main=숫자를 입력해주세요 :
 
-
-
+if  "%main%" == "1" goto _help
+if  "%main%" == "2" goto _package
+if  "%main%" == "3" goto _filter
+if  "%main%" == "4" goto _filesave
+if  "%main%" == "5" goto _EXIT
 
 if %main% LSS 1 ( 
 	echo 잘못 입력하셨습니다.
 	pause
 	goto main
 )
-if %main% GTR 7 (
+if %main% GTR 5 (
 	echo 잘못 입력하셨습니다.
 	pause
 	goto main
 )
+
+:_help
+cls
