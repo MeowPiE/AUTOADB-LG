06fa1e766562b087226fe8f97cccc2682af1de54
backryun <sungbin0718@naver.com>
2019-06-22 17:25:29 +0900

    add devices resolution

--- Script/resolution.bat
+++ Script/resolution.bat
@@ -1,9 +1,35 @@
 @echo off
 :_resolution
 cls
-TITLE resolution v 1.1
+TITLE resolution v 1.8
 echo 경고! 이 기능을 사용시 일부 앱은 오작동 할 수 있습니다.
 echo 이 기능을 사용해서 오작동 하더라도 복구는 가능합니다.
+echo.
+echo 기기를 선택해주세요
+echo.
+echo 1.G6 2.V30 3.V35 4.G7 5.V40 6.G8 7.V50
+echo.
+set /p main=숫자를 입력해주세요 :
+
+if "%main%" == "1" goto _G6resolution
+if "%main%" == "2" goto _V30resolution
+if "%main%" == "3" goto _V35resolution
+if "%main%" == "4" goto _G7resolution
+if "%main%" == "5" goto _V40resolution
+if "%main%" == "6" goto _G8resolution
+if "%main%" == "7" goto _V50resolution
+if %main% LSS 1 ( 
+	echo 잘못 입력하셨습니다.
+	pause
+	goto main
+)
+if %main% GTR 7 (
+	echo 잘못 입력하셨습니다.
+	pause
+	goto main
+)
+
+:_G6resolution
 echo.
 echo 1.해상도 1440p(기본해상도)
 echo.
@@ -15,16 +41,13 @@ set /p main2=원하는 해상도 숫자를 선택해주세요:
 if %main2% LSS 1 ( 
 	echo 잘못 입력하셨습니다.
 	pause
-	goto _resolution
+	goto _G6resolution
 )
 if %main2% GTR 3 (
 	echo 잘못 입력하셨습니다.
 	pause
-	goto _resolution
+	goto _G6resolution
 )
-
-cls
-:ChangeResolution
 	echo 해상도 조절중
 	if "%main2%" == "1" (
 	    Bin\ADB\adb.exe shell wm size 1440x2880
@@ -38,6 +61,45 @@ cls
 	
 	if "%main2%" == "3" (
 	    Bin\ADB\adb.exe shell wm size 720x1440
+		Bin\ADB\adb.exe shell wm density 282
+	)
+)
+
+echo 작업이 완료되었습니다! 아무키나 누르면 초기화면으로 돌아갑니다.
+pause
+
+:_G7resolution
+echo.
+echo 1.해상도 1440p(기본해상도)
+echo.
+echo 2.해상도 1080p
+echo.
+echo 3.해상도 720p
+echo.
+set /p main2=원하는 해상도 숫자를 선택해주세요:
+if %main2% LSS 1 ( 
+	echo 잘못 입력하셨습니다.
+	pause
+	goto _G7resolution
+)
+if %main2% GTR 3 (
+	echo 잘못 입력하셨습니다.
+	pause
+	goto _G7resolution
+)
+	echo 해상도 조절중
+	if "%main2%" == "1" (
+	    Bin\ADB\adb.exe shell wm size 1440x3140
+	    Bin\ADB\adb.exe shell wm density 564
+	    )
+
+	if "%main2%" == "2" (
+	    Bin\ADB\adb.exe shell wm size 1080x2340
+	    Bin\ADB\adb.exe shell wm density 424
+	)
+	
+	if "%main2%" == "3" (
+	    Bin\ADB\adb.exe shell wm size 720x1560
 		Bin\ADB\adb.exe shell wm density 282
 	)
 )
